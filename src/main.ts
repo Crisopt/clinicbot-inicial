91e7fc83fff6a66bd6426d8681def1fdb2950d96
Óticas Maior <bressanini.ofc@gmail.com>
2026-05-26 12:18:51 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.enableCors();
+
   const port = process.env.PORT || 3000;
-  await app.listen(port);
-  console.log(`🚀 ClinicBot API rodando na porta ${port}`);
+
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`🚀 Application running on port ${port}`);
 }
+
 bootstrap();
